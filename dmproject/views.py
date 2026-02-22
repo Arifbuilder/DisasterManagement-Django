61282d2be3015ff54fc0d4ba1a14121c6dcd3802
arif <ashikarifashik8@gmail.com>
2026-02-22 01:27:35 -0800

    Initial Django disaster management project

---
+++ dmproject/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'indro.html')
+
+def signup(request):
+    return render(request,'signup.html')
+
+def login(request):
+    return render(request,'login.html')
